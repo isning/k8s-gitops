9234d089ad042b5a076fdd4616c745ed0ab18110
ISNing <isning_sf@outlook.com>
2026-04-28 22:04:41 +0800

    chore: keep only logto, k6-loadtest and oauth2-proxy, remove all other apps

--- apps/base/istio-bookinfo/download.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/istio/istio/release-1.26/samples/bookinfo/platform/kube/bookinfo.yaml
-wget https://raw.githubusercontent.com/istio/istio/release-1.26/samples/bookinfo/platform/kube/bookinfo-versions.yaml
-wget https://raw.githubusercontent.com/istio/istio/release-1.26/samples/bookinfo/gateway-api/bookinfo-gateway.yaml
