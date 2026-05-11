57fb7aa3f9732a713ef91c8c5d4a2d1f37044615
ISNing <isning_sf@outlook.com>
2026-05-11 14:32:05 +0800

    feat(harbor): add k8s robot account with terraform

---
+++ infra/configs/base/harbor/tf/robot_accounts.tf
@@ -0,0 +1,25 @@
+resource "harbor_robot_account" "k8s" {
+  name   = "k8s"
+  level  = "system"
+
+  secret_wo         = var.robot_account_k8s_secret
+  secret_wo_version = parseint(substr(md5(var.robot_account_k8s_secret), 0, 8), 16)
+
+  dynamic "permissions" {
+    for_each = harbor_project.proxy_cache
+    content {
+      kind      = "project"
+      namespace = permissions.value.name
+
+      access { action = "pull", resource = "repository" }
+      access { action = "list", resource = "repository" }
+
+      access { action = "read", resource = "artifact" }
+      access { action = "list", resource = "artifact" }
+      
+      access { action = "read", resource = "helm-chart" }
+      
+      access { action = "list", resource = "tag" }
+    }
+  }
+}
