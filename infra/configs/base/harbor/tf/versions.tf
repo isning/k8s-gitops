b187a9a3d4927fe56a53b40ebc7e7adabd47d958
ISNing <33572057+isning@users.noreply.github.com>
2026-05-14 17:46:18 +0800

    fix(harbor): use realfile instead of symlink for versions.tf (#363)

--- infra/configs/base/harbor/tf/versions.tf
+++ infra/configs/base/harbor/tf/versions.tf
@@ -1,1 +1,10 @@
-../../../../../tf/versions.tf
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    harbor = {
+      source  = "goharbor/harbor"
+      version = "~> 3.10"
+    }
+  }
+}
