42e92c4c73377d067a9340ad128286420c0d0559
ISNing <isning_sf@outlook.com>
2026-09-09 02:41:11 +0800

    feat: deploy Coder with protected EdgeOne ingress

---
+++ apps/base/coder/tf/sync/providers.tf
@@ -0,0 +1,9 @@
+provider "kubernetes" {
+  host                   = "https://kubernetes.default.svc"
+  token                  = try(file("/var/run/secrets/kubernetes.io/serviceaccount/token"), null)
+  cluster_ca_certificate = try(file("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"), null)
+}
+
+provider "tencentcloud" {
+  domain = "tencentcloudapi.com"
+}
---
+++ apps/base/coder/tf/sync/variables.tf
@@ -0,0 +1,21 @@
+variable "edgeone_zone_id" {
+  description = "EdgeOne site ID containing isning.moe"
+  type        = string
+}
+
+variable "edgeone_cname" {
+  description = "CNAME allocated to the EdgeOne acceleration domain"
+  type        = string
+}
+
+variable "acceleration_domain" {
+  description = "Wildcard Coder workspace acceleration domain"
+  type        = string
+  default     = "*.coder.isning.moe"
+}
+
+variable "gateway_namespace" {
+  description = "Namespace of the dedicated Coder Gateway"
+  type        = string
+  default     = "prod"
+}
